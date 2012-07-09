a35855b7a21f455ebd630d5353040cbac6e4d046
Jessica Janiuk <jessica.janiuk@gmail.com>
2012-07-09 16:57:17 -0500

    Added routing

--- gophers/routes/routes.go
+++ gophers/routes/routes.go
@@ -1,10 +1,36 @@
 package routes
 
-import(
-	"gophers/helpers"
+import (
 	"net/http"
 )
 
-func Home(w http.ResponseWriter, r *http.Request){
-	helpers.DisplayTemplate("index", "templates/index.html", w, make(map[string]interface{}))
+func start() *RouteTable {
+	rt := &RouteTable{}
+	//      var rt = &Route {
+	//          Name: "default",
+	//          Pattern: "/{controller}/{action}/{id}",
+	//			Method: "GET",
+	//          Default: map[string]string { "controller": "home", "action": "index", "id": "0", },
+	//          Constraint: map[string]string { "id": "\\d+" }
+	//      }
+
+	rt.AddRoute(&Route{
+		Name:       "default",
+		Pattern:    "/{controller}/{action}/{id}",
+		Method:     "GET",
+		Default:    map[string]string{"controller": "home", "action": "index", "id": "0"},
+		Constraint: map[string]string{"id": "\\d+"},
+	})
+
+	return rt
 }
+
+func HandleRoute(w http.ResponseWriter, r *http.Request) {
+	rt := &RouteTable{}
+	rt = start()
+	rt.Match(w, r)
+}
+
+/*func Home(w http.ResponseWriter, r *http.Request) {
+	helpers.DisplayTemplate("index", "templates/index.html", w, make(map[string]interface{}))
+}*/

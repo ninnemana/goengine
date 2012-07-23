53a01093a42ac445e086d2cb32cf1571a2b1063c
Alex Ninneman <ninnemana@gmail.com>
2012-07-23 16:03:37 -0500

    merged the routes and controllers packages into routes, now we have access to WebContext from our action

---
+++ gophers/routes/home.go
@@ -0,0 +1,18 @@
+package routes
+
+func homeActions(ctx WebContext) map[string]*Action {
+
+	actionlist := make(map[string]*Action)
+
+	actionlist["index"] = &Action{
+		Name: "index",
+		Run:  homeIndex(ctx),
+	}
+
+	return actionlist
+}
+
+func homeIndex(ctx WebContext) map[string]interface{} {
+	vb := make(map[string]interface{})
+	return vb
+}

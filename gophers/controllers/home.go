ac664c1d5d37429a9da7610752ffb25e0dac42cf
Alex Ninneman <ninnemana@gmail.com>
2012-07-25 09:49:51 -0500

    added sinatra routing capabilities

--- gophers/controllers/home.go
+++ gophers/controllers/home.go
@@ -1,18 +1,10 @@
-package gophers
+package controllers
 
-func homeActions(ctx WebContext) map[string]*Action {
+import (
+	"fmt"
+	"net/http"
+)
 
-	actionlist := make(map[string]*Action)
-
-	actionlist["index"] = &Action{
-		Name: "index",
-		Run:  homeIndex(ctx),
-	}
-	return actionlist
-}
-
-func homeIndex(ctx WebContext) map[string]interface{} {
-
-	vb := make(map[string]interface{})
-	return vb
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello world!")
 }
--- gophers/index.go
+++ gophers/index.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"gophers/controllers"
+	"gophers/routes"
 	"net/http"
 )
 
 func init() {
-	http.HandleFunc("/", gophers.HandleRoute)
+	mux := routes.New()
+
+	mux.Get("/", controllers.Index)
+	http.Handle("/", mux)
 }

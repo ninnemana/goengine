e0fee46dd62b3540328931ed3b9b5c5da3e9cb94
Alex Ninneman <ninnemana@gmail.com>
2012-07-25 08:43:57 -0500

    fixed routing issue

--- gophers/controllers/home.go
+++ gophers/controllers/home.go
@@ -3,7 +3,6 @@ package gophers
 func homeActions(ctx WebContext) map[string]*Action {
 
 	actionlist := make(map[string]*Action)
-
 	actionlist["index"] = &Action{
 		Name: "index",
 		Run:  homeIndex(ctx),

19aae9033e36253fe25fa35ce78c0477ed3adf5f
ninnemana <ninnemana@gmail.com>
2013-06-10 11:55:59 -0500

    added some helper utilities

--- helpers/plate/server.go
+++ helpers/plate/server.go
@@ -123,7 +123,7 @@ func (this *Server) AddRoute(method string, pattern string, handler http.Handler
 		if strings.HasPrefix(part, ":") {
 			expr := "([^/]+)"
 			//a user may choose to override the defult expression
-			// similar to expressjs: ‘/user/:id([0-9]+)’ 
+			// similar to expressjs: ‘/user/:id([0-9]+)’
 			if index := strings.Index(part, "("); index != -1 {
 				expr = part[index:]
 				part = part[:index]
@@ -457,7 +457,7 @@ type ServerConfig struct {
 	RecoverPanic bool
 }
 
-func webTime(t time.Time) string {
+func WebTime(t time.Time) string {
 	ftime := t.Format(time.RFC1123)
 	if strings.HasSuffix(ftime, "UTC") {
 		ftime = ftime[0:len(ftime)-3] + "GMT"
@@ -465,7 +465,7 @@ func webTime(t time.Time) string {
 	return ftime
 }
 
-func dirExists(dir string) bool {
+func DirExists(dir string) bool {
 	d, e := os.Stat(dir)
 	switch {
 	case e != nil:
@@ -477,7 +477,7 @@ func dirExists(dir string) bool {
 	return true
 }
 
-func fileExists(dir string) bool {
+func FileExists(dir string) bool {
 	info, err := os.Stat(dir)
 	if err != nil {
 		return false
@@ -486,6 +486,8 @@ func fileExists(dir string) bool {
 	return !info.IsDir()
 }
 
+// Urlencode is a helper method that converts a map into URL-encoded form data.
+// It is a useful when constructing HTTP POST requests.
 func Urlencode(data map[string]string) string {
 	var buf bytes.Buffer
 	for k, v := range data {
@@ -496,6 +498,41 @@ func Urlencode(data map[string]string) string {
 	}
 	s := buf.String()
 	return s[0 : len(s)-1]
+}
+
+var slugRegex = regexp.MustCompile(`(?i:[^a-z0-9\-_])`)
+
+// Slug is a helper function that returns the URL slug for string s.
+// It's used to return clean, URL-friendly strings that can be
+// used in routing.
+func Slug(s string, sep string) string {
+	if s == "" {
+		return ""
+	}
+	slug := slugRegex.ReplaceAllString(s, sep)
+	if slug == "" {
+		return ""
+	}
+	quoted := regexp.QuoteMeta(sep)
+	sepRegex := regexp.MustCompile("(" + quoted + "){2,}")
+	slug = sepRegex.ReplaceAllString(slug, sep)
+	sepEnds := regexp.MustCompile("^" + quoted + "|" + quoted + "$")
+	slug = sepEnds.ReplaceAllString(slug, "")
+	return strings.ToLower(slug)
+}
+
+// NewCookie is a helper method that returns a new http.Cookie object.
+// Duration is specified in seconds. If the duration is zero, the cookie is permanent.
+// This can be used in conjunction with ctx.SetCookie.
+func NewCookie(name string, value string, age int64) *http.Cookie {
+	var utctime time.Time
+	if age == 0 {
+		// 2^31 - 1 seconds (roughly 2038)
+		utctime = time.Unix(2147483647, 0)
+	} else {
+		utctime = time.Unix(time.Now().Unix()+age, 0)
+	}
+	return &http.Cookie{Name: name, Value: value, Expires: utctime}
 }
 
 func Serve404(w http.ResponseWriter, error string) {
